51955e9cc35bd166316b0a9c146437a2d1a7b879
Candela Colombo <135880086+ccolombo95@users.noreply.github.com>
2025-10-26 21:40:30 -0300

    flechas de projects y overflow page

--- src/components/Icons/ArrowIcon.jsx
+++ src/components/Icons/ArrowIcon.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { useTheme } from '../../context/ThemeContext';
-import arrowBGDark from '../../assets/icons/arrow-bg-dark.svg';
-import arrowBGLight from '../../assets/icons/arrow-bg-light.svg';
+import React from "react";
+import { useTheme } from "../../context/ThemeContext";
+import arrowBGDark from "../../assets/icons/arrow-bg-dark.svg";
+import arrowBGLight from "../../assets/icons/arrow-bg-light.svg";
 
-const ArrowIcon = ({ direction = 'next' }) => {
+const ArrowIcon = ({ direction = "next" }) => {
   const { mode } = useTheme();
-  
+
   const arrows = {
     dark: arrowBGDark,
     light: arrowBGLight,
   };
-  
-  const rotation = direction === 'next' ? 0 : 180;
+
+  const rotation = direction === "next" ? 180 : 0;
 
   return (
-    <img 
-      src={arrows[mode]} 
+    <img
+      src={arrows[mode]}
       alt={`arrow ${direction}`}
       style={{ transform: `rotate(${rotation}deg)` }}
     />
